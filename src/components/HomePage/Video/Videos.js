b80bbbb26c02a9f6af9f35fc2a6a12941de8fe4f
HadeerAIA <66886721+HadeerAIA@users.noreply.github.com>
2022-04-23 01:17:58 +0200

    First Part of HomePage and the Navbar component

---
+++ src/components/HomePage/Video/Videos.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Videos.css'
+import {AiOutlineSearch} from 'react-icons/ai'
+import Video from '../../../assets/egypt2.mp4';
+
+function Videos() {
+    return (
+        <div className='hero'>
+            <video autoPlay loop muted id='video'>
+                <source src={Video} type='video/mp4' />
+            </video>
+            <div className="overlay"></div>
+            <div className="content">
+                <h1>First Class Travel</h1>
+                <h2>Top 1% Locations In Egypt</h2>
+                <form className="form">
+                    <div>
+                        <input type="text" placeholder='Search Destinations' />
+                    </div>
+                    <div>
+                        <button><AiOutlineSearch className='icon'/></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Videos
