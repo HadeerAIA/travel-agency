e5651b7a55aefc301fd0df59e5757cdb39007c1a
HadeerAIA <66886721+HadeerAIA@users.noreply.github.com>
2022-07-06 09:45:45 +0200

    add the search api

--- src/components/HomePage/Video/Videos.js
+++ src/components/HomePage/Video/Videos.js
@@ -2,9 +2,11 @@ import React from "react";
 import {  useState } from "react";
 import "./Videos.css";
 import Video from "../../../assets/egypt2.mp4";
-// import date from ./data.json  // please import the json file here 
+
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=7a1c19ea3c361a4d3cc53eb70ef8298c&query=";
+  "http://localhost:8200/Services/Filters.asmx?op=LOC_Governorates_Sp_Qry"
+// const SEARCH_API =
+//   "https://api.themoviedb.org/3/search/movie?api_key=7a1c19ea3c361a4d3cc53eb70ef8298c&query=";
 
 function Videos() {
   const [searchTerm, setsearchTerm] = useState("");
@@ -12,15 +14,40 @@ function Videos() {
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    if (searchTerm) {
-      fetch(SEARCH_API + searchTerm)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        });
+    const argu = {
+      Data: ['']
+    };
+    const requestOptions = {
+        method: 'POST',
+        dataType: "text; charset=utf-8",
+        mode: 'no-cors', 
+        headers: { 
+          'Content-Type': 'json',
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Control-Allow-Credentials': 'true',
+          'POST': 'OPTIONS'
+        },
+        body: JSON.stringify(argu)
+    };
+
+    fetch(SEARCH_API, requestOptions)
+    .catch((error) => {
+        console.error(error);
+    });
+    // if (searchTerm) {
+    //   fetch(SEARCH_API + searchTerm,{ headers: { 
+    //     'Content-Type': 'json',
+    //     'Access-Control-Allow-Origin': 'http://localhost:3000',
+    //     'Access-Control-Allow-Credentials': 'true',
+    //     'POST': 'OPTIONS'
+    //   }})
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //     });
 
       setsearchTerm("");
-    }
+    
   };
   const handleOnChange = (e) => {
     setsearchTerm(e.target.value);
